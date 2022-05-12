8b21decb30799c31d3e535ad70a60147108e3d63
Nikolai Semko <nisemko@gmail.com>
2022-05-12 10:37:17 -0400

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -6,7 +6,7 @@ import { Switch } from "./DWLib/Switch.js";
 import { CheckBox } from "./DWLib/Check.js";
 import { TextLine } from "./DWLib/SvgText.js";
 
-let total = 50;
+let total = 30;
 let wa;
 let sl = [];
 let logo = [];
